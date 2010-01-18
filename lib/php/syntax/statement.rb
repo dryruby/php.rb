aa884e400439c51848f059248096db9e40c4a7fa
Arto Bendiken <arto@bendiken.net>
2010-01-18 14:27:20 +0100

    Implemented PHP::Statement#to_php.

--- lib/php/syntax/statement.rb
+++ lib/php/syntax/statement.rb
@@ -2,6 +2,12 @@ module PHP
   ##
   # @see http://php.net/manual/en/language.expressions.php
   class Statement < Expression
-    # TODO
+    ##
+    # Returns the PHP representation of this statement.
+    #
+    # @return [String]
+    def to_php
+      "#{super};"
+    end
   end
 end
