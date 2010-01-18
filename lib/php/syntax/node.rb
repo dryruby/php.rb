a5ce330c9ce833a476f65951ed8545a44a54513e
Arto Bendiken <arto@bendiken.net>
2010-01-18 13:03:23 +0100

    Implemented PHP::Node#to_php.

--- lib/php/syntax/node.rb
+++ lib/php/syntax/node.rb
@@ -1,6 +1,15 @@
 module PHP
   ##
   class Node
-    # TODO
+    ##
+    # Returns the PHP representation of this AST node.
+    #
+    # @return [String]
+    # @abstract
+    def to_php
+      raise NotImplementedError
+    end
+
+    alias_method :to_s, :to_php
   end
 end
