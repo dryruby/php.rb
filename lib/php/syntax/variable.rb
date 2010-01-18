6e129200fca14a69d162ce6bb66201c85959107f
Arto Bendiken <arto@bendiken.net>
2010-01-18 14:02:14 +0100

    Implemented PHP::Variable.valid_name?, with specs.

--- lib/php/syntax/variable.rb
+++ lib/php/syntax/variable.rb
@@ -1,14 +1,30 @@
 module PHP
   ##
   class Variable < Expression
+    SYNTAX = /^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$/
+
+    ##
+    # Returns `true` if `name` is a valid PHP variable name.
+    #
+    # @param  [Symbol, #to_s] name
+    # @return [Boolean]
+    def self.valid_name?(name)
+      !(name.to_s =~ SYNTAX).nil?
+    end
+
+    ##
+    # @return [Symbol]
     attr_accessor :name
 
     ##
     # @param  [Symbol, #to_s] name
     def initialize(name)
       @name = case name
-        when Symbol then name
-        else name.to_s.to_sym
+        when Variable
+          name.to_php
+        else
+          raise ArgumentError.new("invalid variable name: #{name.inspect}") unless self.class.valid_name?(name)
+          name.to_s.to_sym
       end
     end
 
--- spec/php_syntax_spec.rb
+++ spec/php_syntax_spec.rb
@@ -2,12 +2,13 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe PHP::Identifier do
   context "when created" do
-    it "should require a name argument" do
+    it "should require an identifier name" do
       lambda { PHP::Identifier.new }.should raise_error(ArgumentError)
       lambda { PHP::Identifier.new(:foo) }.should_not raise_error(ArgumentError)
     end
 
-    it "should convert the name argument to a symbol" do
+    it "should convert the identifier name to a symbol" do
+      PHP::Identifier.new(:foo).name.should be_a_kind_of(Symbol)
       PHP::Identifier.new('foo').name.should be_a_kind_of(Symbol)
       PHP::Identifier.new(123).name.should be_a_kind_of(Symbol)
     end
@@ -23,7 +24,7 @@ end
 
 describe PHP::Literal do
   context "when created" do
-    it "should require a value argument" do
+    it "should require a literal value" do
       lambda { PHP::Literal.new }.should raise_error(ArgumentError)
       lambda { PHP::Literal.new(123) }.should_not raise_error(ArgumentError)
     end
@@ -64,14 +65,26 @@ end
 
 describe PHP::Variable do
   context "when created" do
-    it "should require a value argument" do
+    it "should require a variable name" do
       lambda { PHP::Variable.new }.should raise_error(ArgumentError)
       lambda { PHP::Variable.new(:foo) }.should_not raise_error(ArgumentError)
     end
 
-    it "should convert the name argument to a symbol" do
+    it "should reject invalid variable names" do
+      lambda { PHP::Variable.new(123) }.should raise_error(ArgumentError)
+      lambda { PHP::Variable.new('4site') }.should raise_error(ArgumentError)
+      lambda { PHP::Variable.new('$site') }.should raise_error(ArgumentError)
+    end
+
+    it "should accept valid variable names" do
+      lambda { PHP::Variable.new('var') }.should_not raise_error(ArgumentError)
+      lambda { PHP::Variable.new('Var') }.should_not raise_error(ArgumentError)
+      lambda { PHP::Variable.new('_4site') }.should_not raise_error(ArgumentError)
+    end
+
+    it "should convert the variable name to a symbol" do
+      PHP::Variable.new(:foo).name.should be_a_kind_of(Symbol)
       PHP::Variable.new('foo').name.should be_a_kind_of(Symbol)
-      PHP::Variable.new(123).name.should be_a_kind_of(Symbol)
     end
   end
 
