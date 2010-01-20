e8e14a5e98d571e991a2730d0f244b479d95c63a
Arto Bendiken <arto@bendiken.net>
2010-01-20 19:18:34 +0100

    Imported foreach() examples.

---
+++ doc/examples/foreach1.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+require 'php'
+
+PHP.eval do
+  for fruit in ['apple', 'banana', 'cranberry']
+    echo "#{fruit}\n"
+  end
+end
---
+++ doc/examples/foreach2.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+require 'php'
+
+PHP.eval do
+  for key, value in {'a' => 1, 'b' => 2, 'c' => 3}
+    echo "#{key} => #{value}\n"
+  end
+end
